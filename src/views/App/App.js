925d906476e75140795eaf072c1d71444aa818f6
simonaSerseniuc <simona.serseniuc@gmail.com>
2021-09-13 13:59:14 +0300

    adding routing
    adding routing

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Header from '../../components/Header/Header.js';
 import Wall from '../Wall/Wall.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <Header/>
-            <Wall/>
+            <Router>
+                <Route>
+                    <Switch>
+                        <Redirect exact from="/" to="/feed" />
+                        <Route path="/feed">
+                            <Wall />
+                        </Route>
+                    </Switch>
+                </Route>
+            </Router>
         </div>
     );
 }
